7b96b1dfc802b50e89a0aefbc51165d74b240996
dotthenai <dotthenai@gmail.com>
2015-11-15 21:13:37 -0600

    minor details

--- js/my-animation-scroll.js
+++ js/my-animation-scroll.js
@@ -1,38 +1,38 @@
 if (screen.width >= 768) {
-$(document).ready(function () {
+    $(document).ready(function () {
 
-    $(window).scroll(function() {
+        $(window).scroll(function () {
 
-        var verticalScroll = $(this).scrollTop();
+            var verticalScroll = $(this).scrollTop();
 
-        if(verticalScroll >= 1300) {
+            if (verticalScroll >= 1300) {
 
-            $('#food').addClass('fadeInLeft');
-            $('#food').removeClass('hide-me');
-        }
-        
-        if(verticalScroll >=2300) {
-            
-            $('#food').removeClass('fadeInLeft');
-            $('#food').addClass('hide-me');
-        
-        }
-        
-        if(verticalScroll <=1300) {
-            
-            $('#food').removeClass('fadeInLeft');
-            $('#food').addClass('hide-me');
-        
-        }
+                $('#food').addClass('fadeInLeft');
+                $('#food').removeClass('hide-me');
+            }
 
+            if (verticalScroll >= 2300) {
+
+                $('#food').removeClass('fadeInLeft');
+                $('#food').addClass('hide-me');
+
+            }
+
+            if (verticalScroll <= 1300) {
+
+                $('#food').removeClass('fadeInLeft');
+                $('#food').addClass('hide-me');
+
+            }
+
+        });
     });
-});
 }
 
 
 
 
 
-$('.nav a').on('click', function(){
+$('.nav a').on('click', function () {
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
 });
